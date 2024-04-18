577c7d595ec57c77a52e2b9e317b44b803c73735
Aary <71329841+Aarylitics@users.noreply.github.com>
2024-04-18 17:29:26 -0400

    Update streamlit_grades.py

--- streamlit_grades.py
+++ streamlit_grades.py
@@ -18,7 +18,7 @@ st.sidebar.image("agilogo(1).jpeg", use_column_width=True)
 st.sidebar.title('Agricultural Institute Dashboard')
 st.sidebar.divider()
 
-st.markdown("# Student Assessment")
+st.markdown("# Student Assessment Model")
 st.markdown("### From seeing current grade distributions to obtaining a list on students that need academic help, this dashboard will help you do just that!")
 
 
